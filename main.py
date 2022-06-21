7fddf16d70a7f31ef010a2312422c2d651ecf2c9
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-06-21 17:17:54 -0600

    finish print instead of twilio(no longer free)

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-av_API_KEY = '3NZV2MQX9PDA0X36'
-BASE_URL = 'https://www.alphavantage.co/query?'
+STOCK_API_KEY = '3NZV2MQX9PDA0X36'
+STOCK_URL = 'https://www.alphavantage.co/query?'
 query = {
     'function': 'TIME_SERIES_DAILY',
     'symbol': 'TSLA',
     'apikey': '3NZV2MQX9PDA0X36'
 }
 
-r = requests.get(BASE_URL, params=query)
+r = requests.get(STOCK_URL, params=query)
 r.raise_for_status()
 data = r.json()["Time Series (Daily)"]
 
@@ -25,17 +25,49 @@ yesterday_2x_close = float(data_list[1]['4. close'])
 difference = abs(yesterday_close - yesterday_2x_close)
 percent_diff = (difference / yesterday_close) * 100
 
-if percent_diff >= 5:
-    print("Get News")
-
 
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 2
+NEWS_API_KEY = 'b6c2023fd9cf4013a0684fa0627e3c38'
+NEWS_URL = 'https://newsapi.org/v2/top-headlines?'
+news_query = {
+    'q': 'Tesla',
+    'sortBy': 'publishedAt',
+    'apiKey': NEWS_API_KEY
+}
+news_r = requests.get(NEWS_URL, params=news_query)
+news_data = news_r.json()['articles']
+news_data_top3 = [
+                [news_data[0]['title'], news_data[0]['description']], 
+                [news_data[0]['title'], news_data[0]['description']], 
+                [news_data[0]['title'], news_data[0]['description']]
+                  ]
+
+# if percent_diff >= 5:
+#     print(news_data[0]['title'])
+#     print(news_data[1]['title'])
+#     print(news_data[2]['title'])
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
+'''Twilio no longer free service - continuing final step with print statement'''
 
+up_down = None
+if (yesterday_close - yesterday_2x_close) > 0:
+    up_down = '🔼'
+else:
+    up_down = '🔻'
 
+if percent_diff >= 5:
+    for ea in news_data_top3:
+        print(f"""
+              {STOCK}: {up_down}%
+              Headline: {ea[0]}
+              Brief: {ea[1]}
+              """)
+        
+        
 #Optional: Format the SMS message like this: 
 """
 TSLA: 🔺2%
